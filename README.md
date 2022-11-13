408ca99e8eb24fe5256318cd0416995ba5e42d0c
kobakoba5884 <kobanminmin58@gmail.com>
2022-11-13 16:43:53 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+<h1 align="center">java</h1>
+
+
+ 
---
+++ aws-practice/app/src/main/java/aws/practice/auroraPostgres/CredentioalsInfo.java
@@ -0,0 +1,11 @@
+package aws.practice.auroraPostgres;
+
+public class CredentioalsInfo {
+    public static final String url = "";
+    public static final String user = "";
+    public static final String password = "";
+
+    private CredentioalsInfo(){
+    }
+
+}
