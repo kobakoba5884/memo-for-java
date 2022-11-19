1dbf2dbbc186731b7ac959f756c334ed31b4a77f
kobakoba5884 <kobanminmin58@gmail.com>
2022-11-19 18:16:02 +0900

    add gradle

---
+++ gradle-practice/gradle-tutorial/src/main/java/gradle/tutorial/GradleTutorial.java
@@ -0,0 +1,7 @@
+package gradle.tutorial;
+
+public class GradleTutorial {
+    public static void main(String[] args){
+        System.out.println("Gradle 4tw!");
+    }
+}
---
+++ gradle-practice/gradle-tutorial/src/test/java/gradle/tutorial/GradleTutorialTest.java
@@ -0,0 +1,10 @@
+package gradle.tutorial;
+
+import org.junit.jupiter.api.Test;
+
+public class GradleTutorialTest {
+    @Test
+    void verifyNoExceptionThrown(){
+        GradleTutorial.main(new String[]{});
+    }
+}
